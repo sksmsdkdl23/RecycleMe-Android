62cbe68cc42c53ac340ebc96d2d39309199c0a1d
Micole Ong <micoleinah@gmail.com>
2018-03-11 05:04:07 -0700

    Added layout for total page

--- app/src/main/java/com/hackuvic/twoblocksaway/recycleme/Total/TotalActivity.java
+++ app/src/main/java/com/hackuvic/twoblocksaway/recycleme/Total/TotalActivity.java
@@ -3,6 +3,8 @@ package com.hackuvic.twoblocksaway.recycleme.Total;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hackuvic.twoblocksaway.recycleme.R;
+
 public class TotalActivity extends AppCompatActivity {
 
     @Override
